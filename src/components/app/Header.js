3f6142e9f4675bacdf574b63b5124e879d651a43
José Villegas <super.jose.bros@icloud.com>
2024-11-27 14:36:01 -0500

    Add styles for post cards and post list grid layout

---
+++ src/components/app/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="logo">Logo</div>
+            <nav>
+                <ul>
+                    <li>Account</li>
+                    <li>Settings</li>
+                    <li>Help</li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
